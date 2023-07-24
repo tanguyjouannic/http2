56705adff95c8da341d81f1f92934c6dcbd07af7
Tanguy Jouannic <tanguy.jouannic@gmail.com>
2023-07-24 21:13:12 +0200

    Fixes the Http2Integer encoding.

--- src/primitive.rs
+++ src/primitive.rs
@@ -2,7 +2,7 @@ use std::result;
 
 use crate::error::Http2Error;
 
-/// Http2 Integer Primitive.
+/// HTTP/2 Integer Primitive.
 ///
 /// Integers are used to represent name indexes, header field indexes, or
 /// string lengths. An integer representation can start anywhere within
@@ -49,82 +49,104 @@ impl Http2Integer {
         Http2Integer { value }
     }
 
-    /// Encode an Http2Integer.
-    ///
-    /// Pseudocode to represent an integer I is as follows:
-    ///
-    /// if I < 2^N - 1, encode I on N bits
-    /// else
-    ///     encode (2^N - 1) on N bits
-    ///     I = I - (2^N - 1)
-    ///     while I >= 128
-    ///         encode (I % 128 + 128) on 8 bits
-    ///         I = I / 128
-    ///     encode I on 8 bits
+    /// Encode an HTTP/2 Integer.
     ///
     /// # Arguments
     ///
     /// * `n` - The number of bits of the prefix.
     pub fn encode(&self, n: u8) -> Result<Vec<u8>, Http2Error> {
-        // Verify that n < 8.
-        if n > 7 {
+        // Verify that n <= 8 and n != 0.
+        if n > 8 || n == 0 {
             return Err(Http2Error::PrimitiveError(
-                "Invalid prefix length".to_string(),
+                "HTTP/2 Integer prefix must be between 1 and 8 bits.".to_string(),
             ));
         }
 
         // Verify that the value is not empty.
         if self.value.is_empty() {
             return Err(Http2Error::PrimitiveError(
-                "Http2Integer cannot be empty.".to_string(),
+                "Cannot encode an empty HTTP/2 Integer".to_string(),
             ));
         }
 
+        // Calculate the maximum value that can be encoded on the prefix.
+        let max_value = 2u8.pow(n as u32) - 1;
+
         // If the value can be encoded on the prefix, encode it.
-        if self.value.len() == 1 && self.value[0] < 2u8.pow(n as u32) - 1 {
+        if self.value.len() == 1 && self.value[0] < max_value {
             return Ok(vec![self.value[0]]);
         }
 
-        // Encode the value using the prefix and the continuation.
+        // If the value cannot be encoded on the prefix, start the encoding process.
+
+        // Reverse the value to get it in low endian.
         let mut value = self.value.clone();
         value.reverse();
 
+        for v in &value {
+            print!("{:b} ", v);
+        }
+        println!("\n");
+
+        // Subtract the maximum value from the value and take care of the carry.
+        for i in 0..value.len() {
+            match value[i].checked_sub(max_value) {
+                Some(v) => {
+                    value[i] = v;
+                    break;
+                }
+                None => {
+                    value[i] = value[i].overflowing_sub(max_value).0;
+                }
+            }
+        }
+
+        for v in &value {
+            print!("{:b} ", v);
+        }
+
         // Encode the prefix.
         let mut result = Vec::new();
-        result.push(2u8.pow(n as u32) - 1);
+        result.push(max_value);
 
         loop {
-            // Get the first chunk.
+            // Get a chunk.
             let chunk = value[0] & 0x7F;
 
-            // Shift the values.
-            for i in 0..value.len() - 1 {
-                println!("i: {}", i);
-                value[i] = value[i] >> 7;
-                value[i] = value[i] + ((value[i + 1] & 0x7F) << 1);
-            }
-
-            *value.last_mut().unwrap() = value.last().unwrap() >> 7;
-
-            // Check if the last chunk is 0.
-            if value[value.len() - 1] == 0 {
-                value.pop();
-            }
-
-            // If this is the last chunk, set the most significant bit to 0.
+            // Check if it is the last chunk.
             if value.len() == 1 {
                 if value[0] < 128 {
                     result.push(chunk);
                     break;
                 } else {
                     result.push(chunk | 0x80);
-                    result.push(1);
+                    result.push(0x01);
                     break;
                 }
-            } else {
-                result.push(chunk | 0x80);
             }
+
+            // Shift the values.
+            for i in 0..value.len() - 1 {
+                value[i] = value[i] >> 7;
+                value[i] = value[i] + ((value[i + 1] & 0x7F) << 1);
+            }
+
+            *value.last_mut().unwrap() = value.last().unwrap() >> 7;
+
+            // Pop the last value if it is 0.
+            if value[value.len() - 1] == 0 {
+                value.pop();
+            }
+
+            // Push the chunk.
+            result.push(chunk | 0x80);
         }
+
+        println!("\n yo");
+        for v in &result {
+            print!("{:b} ", v);
+        }
+
 
         Ok(result)
     }
--- tests/tests_primitives.rs
+++ tests/tests_primitives.rs
@@ -2,9 +2,18 @@ use http2::primitive::Http2Integer;
 
 #[test]
 pub fn test_encode_integer() {
-    let integer = Http2Integer::new(vec![0b00000101, 0b10011101, 0b10110011]);
+    let integer = Http2Integer::new(vec![0b00000101, 0b10011101, 0b10110011]); // 368051
     let encoded = integer.encode(5).unwrap();
+    assert_eq!(encoded, vec![0b11111, 0b10010100, 0b10111011, 0b10110]); // 
     for byte in encoded {
         print!("{:b} ", byte);
     }
 }
+
+// 101 10011101 10110011 = 368051 big endian
+
+// 101 10011101 10010100 = 368020 big endian
+
+// 10010100 10011101 101 = 368020 little endian
+
+// 10010100 10111011 10110
